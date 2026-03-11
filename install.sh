6cb5e24e9ff735cd0defa44c17595c811d4cb6b1
Fizxyyyy <aakuunff001@gmail.com>
2026-03-11 22:51:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,30 +2,42 @@
 
 echo "=== FIZXY ROBLOX TOOLS ==="
 
+CONFIG="$HOME/.fizxy_config"
+
+if [ -f "$CONFIG" ]; then
+. "$CONFIG"
+echo "[✓] Config ditemukan"
+else
 read -p "Masukkan License Key: " KEY
 read -p "Masukkan Link Private Server: " PS
 read -p "Masukkan Discord Webhook (optional): " WEBHOOK
 
-HWID=$(cat /proc/sys/kernel/random/uuid)
+echo "KEY=\"$KEY\"" > $CONFIG
+echo "PS=\"$PS\"" >> $CONFIG
+echo "WEBHOOK=\"$WEBHOOK\"" >> $CONFIG
+fi
 
+HWID=$(cat /proc/sys/kernel/random/uuid)
 URL="http://n3.panelbot.id:2400/get_script"
 
 echo "[*] Verifikasi ke server..."
 
-RESPONSE=$(curl -s -X POST $URL \
+RESPONSE=$(curl -s -X POST "$URL" \
 -H "Content-Type: application/json" \
 -d "{\"key\":\"$KEY\",\"hwid\":\"$HWID\"}")
 
-SCRIPT=$(echo "$RESPONSE" | sed -n 's/.*"script":"\(.*\)".*/\1/p' | sed 's/\\n/\n/g')
+SCRIPT=$(echo "$RESPONSE" | sed 's/.*"script":"//;s/"}.*//' | sed 's/\\n/\n/g')
 
 if [ -z "$SCRIPT" ]; then
 echo "[X] Key tidak valid atau server error"
 exit
 fi
 
-echo "$SCRIPT" > run.sh
-chmod +x run.sh
+echo "[*] Mengambil script terbaru..."
 
-echo "[✓] Akses diterima, menjalankan script..."
+echo "$SCRIPT" > script_roblox.sh
+chmod +x script_roblox.sh
 
-su -c "sh run.sh \"$PS\" \"$WEBHOOK\""
+echo "[✓] Script terbaru siap"
+
+su -c "sh script_roblox.sh \"$PS\" \"$WEBHOOK\""
