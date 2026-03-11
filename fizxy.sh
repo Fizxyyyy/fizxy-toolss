ee77bdfd508baf89f98ddde3c74c76fa236efcd6
Fizxyyyy <aakuunff001@gmail.com>
2026-03-12 01:13:04 +0700

    Update fizxy.sh

--- fizxy.sh
+++ fizxy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# Loader Fizxy Tools v2
+# Loader Fizxy Tools v2 - Stabilized Input
 
 URL_API="http://n3.panelbot.id:2400/verify"
 
 clear
 echo -e "\033[92m--- FIZXY TOOLS AUTO INSTALLER ---\033[0m"
 
-# Input Data
-read -p "Masukkan License Key: " KEY
-read -p "Link Private Server: " PS
-read -p "Webhook Discord (Opsional): " WH
+# Input Data menggunakan /dev/tty agar tidak terlewati curl
+printf "Masukkan License Key: "
+read KEY < /dev/tty
+printf "Link Private Server: "
+read PS < /dev/tty
+printf "Webhook Discord (Opsional): "
+read WH < /dev/tty
 
 # Ambil ID Device
 HWID=$(getprop ro.serialno)
@@ -25,6 +28,5 @@ elif [[ "$RESPONSE" == "LIMIT" ]]; then
     echo -e "\033[91m[X] Slot Redfinger Penuh (Max 5)!\033[0m"
 else
     echo -e "\033[92m[V] Berhasil! Menjalankan Roblox Manager...\033[0m"
-    # Jalankan script roblox dari panel ke bash, kirim PS dan WH sebagai argumen
     echo "$RESPONSE" | bash -s -- "$PS" "$WH"
 fi
